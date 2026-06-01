0eed4c644abaea508cd044b393ed3cddcac741e5
Picobola1 <bonilla.ellie@gmail.com>
2026-06-01 16:16:04 -0700

    added some assets and made the window the right size and settings. I also tried to start a script

---
+++ tamagotchi-attempt/main.gd
@@ -0,0 +1,13 @@
+extends AnimatedSprite2D
+
+var NewWindow = Window.new()
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var NewWindow = Window.new()
+	NewWindow.
