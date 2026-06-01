60828f87e2d6931386afd7d506a9f1b0ee1807d4
Picobola1 <bonilla.ellie@gmail.com>
2026-06-01 16:43:09 -0700

    2 windows!

--- tamagotchi-attempt/main.gd
+++ tamagotchi-attempt/main.gd
@@ -10,4 +10,11 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var NewWindow = Window.new()
-	NewWindow.
+	add_child(NewWindow)
+	NewWindow.title = "FOOD"
+	NewWindow.position = Vector2i(100,100)
+	NewWindow.visible = true
+	NewWindow.size = Vector2i(128,128)
+	
+	var second_scene = preload("res://scenes/food.tscn").instantiate()
+	NewWindow.add_child(second_scene)
