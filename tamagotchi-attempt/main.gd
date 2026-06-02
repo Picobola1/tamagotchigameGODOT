1aa40d9a539634998583ba5f0ca1c609e82e222c
Picobola1 <bonilla.ellie@gmail.com>
2026-06-01 17:18:39 -0700

    took me way to long to make  the arrow and do the theme

--- tamagotchi-attempt/main.gd
+++ tamagotchi-attempt/main.gd
@@ -4,17 +4,21 @@ var NewWindow = Window.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
 	var NewWindow = Window.new()
 	add_child(NewWindow)
 	NewWindow.title = "FOOD"
 	NewWindow.position = Vector2i(100,100)
 	NewWindow.visible = true
 	NewWindow.size = Vector2i(128,128)
+	NewWindow.unresizable = true
+	NewWindow.maximize_disabled = true
+	NewWindow.minimize_disabled = true
 	
 	var second_scene = preload("res://scenes/food.tscn").instantiate()
 	NewWindow.add_child(second_scene)
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
