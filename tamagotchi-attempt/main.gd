a55dd55e0f7c04e01761b66b56838b31fd072054
Picobola1 <bonilla.ellie@gmail.com>
2026-06-01 17:31:29 -0700

    made a make window function!

--- tamagotchi-attempt/main.gd
+++ tamagotchi-attempt/main.gd
@@ -4,21 +4,27 @@ var NewWindow = Window.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var NewWindow = Window.new()
-	add_child(NewWindow)
-	NewWindow.title = "FOOD"
-	NewWindow.position = Vector2i(100,100)
-	NewWindow.visible = true
-	NewWindow.size = Vector2i(128,128)
-	NewWindow.unresizable = true
-	NewWindow.maximize_disabled = true
-	NewWindow.minimize_disabled = true
 	
-	var second_scene = preload("res://scenes/food.tscn").instantiate()
-	NewWindow.add_child(second_scene)
+	makeWindow("Food", "res://scenes/food.tscn", Vector2i(128,128), Vector2i(10,10))
 
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func makeWindow(title: String,scene: String,size: Vector2i, position: Vector2i):
+	var window = Window.new()
+	add_child(window)
+	window.title = title
+	window.position = Vector2i(100,100)
+	window.visible = true
+	window.size = Vector2i(128,128)
+	window.unresizable = true
+	window.maximize_disabled = true
+	window.minimize_disabled = true
+	# pre load dosent accept vars so we load it imdetly instead
+	var loaded_scene = load(scene)
+	var scene_instance = loaded_scene.instantiate()
+	window.add_child(scene_instance)
+	
