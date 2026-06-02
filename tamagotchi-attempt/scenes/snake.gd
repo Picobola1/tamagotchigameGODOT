0a3c08fdc2aabc9f71a98ec64a8ab670df9cee72
Picobola1 <bonilla.ellie@gmail.com>
2026-06-02 16:23:18 -0700

    YOU CAN MOVE

--- tamagotchi-attempt/scenes/snake.gd
+++ tamagotchi-attempt/scenes/snake.gd
@@ -11,7 +11,7 @@ var snake_data : Array
 var snake : Array
 var cells : int = 20
 var cellSize : int = 50
-var start_pos = Vector2(0,0)
+var start_pos = Vector2(9,9)
 var up = Vector2(0, -1)
 var down = Vector2(0,1)
 var left = Vector2(-1,0)
@@ -31,14 +31,14 @@ func genarate_snake():
 	old_data.clear()
 	snake_data.clear()
 	snake.clear()
-	for i in range(1):
+	for i in range(3):
 		add_segment(start_pos + Vector2(0,i))
 func add_segment(pos):
 	print("making snake at ", pos)
 	snake_data.append(pos)
 	var SnakeSegment = snake_scene.instantiate()
 	add_child(SnakeSegment)
-	SnakeSegment.position = (pos * cellSize)
+	SnakeSegment.position = (pos * cellSize) + Vector2(0,cellSize)
 	
 	snake.append(SnakeSegment)
 	print("START POS IS: ", start_pos)
@@ -47,4 +47,46 @@ func add_segment(pos):
 	print("global screen pos: ", SnakeSegment.global_position)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	move_snake()
+func move_snake():
+	if can_move:
+		if Input.is_action_just_pressed("down") and move_direction != up:
+			move_direction = down
+			can_move = false
+			if not game_start:
+				start_game()
+		if Input.is_action_just_pressed("up") and move_direction != down:
+			move_direction = up
+			can_move = false
+			if not game_start:
+				start_game()
+		if Input.is_action_just_pressed("left") and move_direction != right:
+			move_direction = left
+			can_move = false
+			if not game_start:
+				start_game()
+		if Input.is_action_just_pressed("right") and move_direction != left:
+			move_direction = right
+			can_move = false
+			if not game_start:
+				start_game()
+
+func start_game():
+	game_start = true
+	$MoveTimer.start()
+			
+
+func _on_move_timer_timeout() -> void:
+	
+	can_move = true
+
+	old_data = [] + snake_data
+
+	snake_data[0] += move_direction
+
+	for i in range(len(snake_data)):
+		if i > 0:
+			snake_data[i] = old_data[i - 1]
+
+		snake[i].position = (snake_data[i] * cellSize) + Vector2(0, cellSize)
+	 
